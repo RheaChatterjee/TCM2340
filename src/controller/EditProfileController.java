90d0323a62ac96f48e0b1c73c807f1ad2e5bfa26
elizabethcooper <elizabeth.cooper21@gmail.com>
2016-10-02 14:38:31 -0400

    Added edit Profile screen and controller

---
+++ src/controller/EditProfileController.java
@@ -0,0 +1,24 @@
+package controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+/**
+ * Created by Elizabeth on 10/2/2016.
+ */
+public class EditProfileController {
+    @FXML
+    private TextField Name;
+
+    @FXML
+    private TextField Email;
+
+    @FXML
+    private TextField Password;
+
+    @FXML
+    private TextField addressBox;
+
+    @FXML
+    private TextField titleBox;
+}
