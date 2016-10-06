cad61a681afebc0816206e47d73802c816e50dfe
elizabethcooper <elizabeth.cooper21@gmail.com>
2016-10-06 16:21:44 -0400

    created report class

---
+++ src/model/Report.java
@@ -0,0 +1,18 @@
+package model;
+
+/**
+ * Created by Elizabeth on 10/6/2016.
+ */
+public class Report {
+
+    private long timestamp;
+    private int reportNumber;
+    private String reporterName;
+    private String location;
+    private String waterType;
+
+
+    public Report() {
+
+    }
+}
---
+++ src/model/SubmittedReports.java
@@ -0,0 +1,7 @@
+package model;
+
+/**
+ * Created by Elizabeth on 10/6/2016.
+ */
+public class SubmittedReports {
+}
