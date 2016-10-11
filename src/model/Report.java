d9ad6c3751b107e8b30d9d320786c9aeb5312ff9
elizabethcooper <elizabeth.cooper21@gmail.com>
2016-10-11 12:32:08 -0400

    finished report and submitted reports classes

--- src/model/Report.java
+++ src/model/Report.java
@@ -1,18 +1,101 @@
 package model;
 
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 /**
  * Created by Elizabeth on 10/6/2016.
  */
 public class Report {
 
-    private long timestamp;
-    private int reportNumber;
-    private String reporterName;
-    private String location;
-    private String waterType;
+    private final StringProperty dateTime = new SimpleStringProperty();
+    private final StringProperty reportNumber = new SimpleStringProperty();
+    private final StringProperty reporterName = new SimpleStringProperty();
+    private final StringProperty location = new SimpleStringProperty();
+    private final StringProperty waterType = new SimpleStringProperty();
 
 
     public Report() {
-
+        DateFormat dateFormat = new SimpleDateFormat("MM/dd/yyyy h:mm a");
+        Date date = new Date();
+        this.dateTime.set(date.toString());
     }
+
+    public Report(String dateTime, String reportNumber, String reporterName, String location, String waterType) {
+        setDateTime(dateTime);
+        setReportNumber(reportNumber);
+        setReporterName(reporterName);
+        setLocation(location);
+        setWaterType(waterType);
+        DateFormat dateFormat = new SimpleDateFormat("MM/dd/yyyy h:mm a");
+        Date date = new Date();
+        this.dateTime.set(date.toString());
+    }
+
+    public String getDateTime() {
+        return dateTime.get();
+    }
+
+    public StringProperty dateTimeProperty() {
+        return dateTime;
+    }
+
+    public void setDateTime(String dateTime) {
+        this.dateTime.set(dateTime);
+    }
+
+    public String getReportNumber() {
+        return reportNumber.get();
+    }
+
+    public StringProperty reportNumberProperty() {
+        return reportNumber;
+    }
+
+    public void setReportNumber(String reportNumber) {
+        this.reportNumber.set(reportNumber);
+    }
+
+    public String getReporterName() {
+        return reporterName.get();
+    }
+
+    public StringProperty reporterNameProperty() {
+        return reporterName;
+    }
+
+    public void setReporterName(String reporterName) {
+        this.reporterName.set(reporterName);
+    }
+
+    public String getLocation() {
+        return location.get();
+    }
+
+    public StringProperty locationProperty() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location.set(location);
+    }
+
+    public String getWaterType() {
+        return waterType.get();
+    }
+
+    public StringProperty waterTypeProperty() {
+        return waterType;
+    }
+
+    public void setWaterType(String waterType) {
+        this.waterType.set(waterType);
+    }
+
 }
--- src/model/SubmittedReports.java
+++ src/model/SubmittedReports.java
@@ -1,7 +1,18 @@
 package model;
 
+import java.util.ArrayList;
+
 /**
  * Created by Elizabeth on 10/6/2016.
  */
 public class SubmittedReports {
+    private static final ArrayList<Report> submittedReports = new ArrayList<>();
+
+    public ArrayList getSubmittedReports() {
+        return submittedReports;
+    }
+
+    public void addReport(Report report) {
+        submittedReports.add(report);
+    }
 }
