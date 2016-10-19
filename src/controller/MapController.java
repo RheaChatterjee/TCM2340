b96cbd829a865242877594e8f4ffebbc8f994799
davidjtran <david.tran456@gmail.com>
2016-10-18 21:57:00 -0400

    Google maps api loaded

---
+++ src/controller/MapController.java
@@ -0,0 +1,31 @@
+package controller;
+
+import com.lynden.gmapsfx.GoogleMapView;
+import com.lynden.gmapsfx.MapComponentInitializedListener;
+import com.lynden.gmapsfx.javascript.event.UIEventType;
+import com.lynden.gmapsfx.javascript.object.*;
+
+import cleanWaterApp.MainApp;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.stage.FileChooser;
+import javafx.stage.Window;
+
+
+import netscape.javascript.JSObject;
+
+import java.io.File;
+import java.net.URL;
+import java.util.List;
+import java.util.ResourceBundle;
+
+/**
+ * Created by David on 10/18/2016.
+ */
+public class MapController {
+
+    public MapController() {
+    }
+
+}
